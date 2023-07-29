23c9535afc7f22b094e51a6792c1476f89633a7b
Ayeshi  I. Jayarathna <136877212+AyeshiJayarathna@users.noreply.github.com>
2023-07-29 12:15:33 +0530

    Add files via upload

--- QUE.1.c
+++ QUE.1.c
@@ -1,31 +1,70 @@
-#include<stdio.h>
-int main(){
-int i=0;
-while(i<=100){
-printf("%d",i);
-i++;
-}
-return 0;
-}
+
 #include <stdio.h>
-#include <stdlib.h>
 
-int main()
-{
-    int i=0;
-    do{
-    printf("%d",i);
-    i++;
-    }while(i<=100);
-    return 0;
-}#include <stdio.h>
-#include <stdlib.h>
+int find_minimum(int arr[], int size) {
+    int min_val = arr[0];
+    for (int i = 1; i < size; i++) {
+        if (arr[i] < min_val) {
+            min_val = arr[i];
+        }
+    }
+    return min_val;
+}
 
-int main()
-{
-   int i;
-   for(i=0;i<=100;i++) {
-        printf("%d",i);
-   }
+int find_maximum(int arr[], int size) {
+    int max_val = arr[0];
+    for (int i = 1; i < size; i++) {
+        if (arr[i] > max_val) {
+            max_val = arr[i];
+        }
+    }
+    return max_val;
+}
+
+float find_average(int arr[], int size) {
+    float sum = 0;
+    for (int i = 0; i < size; i++) {
+        sum += arr[i];
+    }
+    return sum / size;
+}
+
+void reverse_array(int arr[], int size) {
+    int temp;
+    for (int i = 0; i < size / 2; i++) {
+        temp = arr[i];
+        arr[i] = arr[size - i - 1];
+        arr[size - i - 1] = temp;
+    }
+}
+
+int main() {
+    int arr[10];
+
+    printf("Enter 10 values for the array:\n");
+    for (int i = 0; i < 10; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+
+    int min_val = find_minimum(arr, 10);
+    printf("Minimum value: %d\n", min_val);
+
+
+    int max_val = find_maximum(arr, 10);
+    printf("Maximum value: %d\n", max_val);
+
+
+    float avg_val = find_average(arr, 10);
+    printf("Average value: %.2f\n", avg_val);
+
+
+    reverse_array(arr, 10);
+    printf("Reversed array: ");
+    for (int i = 0; i < 10; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
     return 0;
 }
--- QUE.2.c
+++ QUE.2.c
@@ -1,28 +1,41 @@
-
- #include <stdlib.h>
 #include <stdio.h>
 
-int main()
-{
-   int marks[10];
-   int total=0;
-   int i;
-   printf("Enter the 10 marks\n:");
-   for(i=0;i<10;i++){
-    printf("marks %:",i+1);
-    scanf("%d",&marks[i]);
-    total+=marks[i];
+int main() {
+    int size;
+    printf("Enter the size of the arrays: ");
+    scanf("%d", &size);
+
+    int array1[size], array2[size], scalarSum = 0;
+    int vectorSum[size];
+
+
+    printf("Enter %d elements for array1:\n", size);
+    for (int i = 0; i < size; i++) {
+        scanf("%d", &array1[i]);
+        scalarSum += array1[i];
     }
-    float average=(float)total/10;
-    printf("Total marks:%d\n");
-    printf("average marks:%d\n",average);
-    if(average<50)
-    {
-    printf("Fail!\n");
+
+
+    printf("Enter %d elements for array2:\n", size);
+    for (int i = 0; i < size; i++) {
+        scanf("%d", &array2[i]);
+        scalarSum += array2[i];
     }
-    else
-    {
-        printf("pass!\n");
+
+
+    for (int i = 0; i < size; i++) {
+        vectorSum[i] = array1[i] + array2[i];
     }
+
+
+    printf("Scalar Sum: %d\n", scalarSum);
+
+
+    printf("Vector Sum: ");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", vectorSum[i]);
+    }
+    printf("\n");
+
     return 0;
 }
